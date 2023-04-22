649b91915805a61e69c02e9f7b6c4bd1c7500244
MX_Master <mikhail@vydrenko.ru>
2023-04-22 08:24:28 +0600

    + history functionality

--- logics.js
+++ logics.js
@@ -1,6 +1,6 @@
 function page_init() {
     // save/restore form values
-    var storage_list = ['min','max','count','unique','sort'];
+    var storage_list = ['min','max','count','unique','unique_digits','sort'];
     for (var i = storage_list.length; i--;) {
         var ls = localStorage.getItem(storage_list[i]);
         var e = document.querySelector('#'+storage_list[i]);
@@ -21,14 +21,34 @@ function page_init() {
             }
         }
     }
-    // do a calulations on button click
+    // add some actions for the buttons
     var e = document.querySelector('#generate');
     if (e && typeof(generate) == 'function') e.addEventListener("click", generate);
+    e = document.querySelector('#clear');
+    if (e && typeof(clear_history) == 'function') e.addEventListener("click", clear_history);
+    e = document.querySelector('#history');
+    if (e && typeof(show_history) == 'function') e.addEventListener("click", show_history);
+    // init vars
+    history_list = [];
+}
+
+function clear_history() {
+    // do an action
+    history_list = [];
+    // show results
+    var e = document.querySelector('#results');
+    if (e) e.innerText = 'history was cleared';
+}
+
+function show_history() {
+    // show results
+    var e = document.querySelector('#results');
+    if (e) e.innerText = 'history: ' + history_list.join(', ');
 }
 
 function generate() {
     // get parameters
-    var p = {'min':0, 'max':0, 'count':0, 'unique':0, 'sort':0};
+    var p = {'min':0, 'max':0, 'count':0, 'unique':0, 'unique_digits':0, 'sort':0};
     for (var name in p) {
         var e = document.querySelector('#'+name);
         if (!e) continue;
@@ -43,6 +63,7 @@ function generate() {
     p.count = Math.abs(Math.round(p.count));
     // get results
     var rnd_list = [];
+    var unique_list = [];
     var fract_digits = 0;
     if ( (''+p.min).indexOf('.') >= 0 )
         fract_digits = (''+p.min).length - (''+p.min).indexOf('.') - 1;
@@ -50,14 +71,32 @@ function generate() {
         fract_digits = Math.max(fract_digits, (''+p.max).length - (''+p.max).indexOf('.') - 1);
     for (var i = p.count, rnd = 0; i--; ) {
         if (p.unique) {
-            for (tries = 1000; tries--; ) {
-                rnd = Math.random() * (p.max - p.min) + p.min;
-                rnd = parseFloat( rnd.toFixed(fract_digits) );
-                if (!rnd_list.includes(rnd)) {rnd_list.push(rnd); break;}
+            if (0/*p.unique_digits*/) {
+                // TODO
+                for (var tries = 1000; tries--; ) {
+                    rnd = Math.random() * (p.max - p.min) + p.min;
+                    rnd = rnd.toFixed(fract_digits);
+                    if (!unique_list.includes(rnd)) {
+                        history_list.push( parseFloat(rnd) );
+                        unique_list.push(''+rnd);
+                        break;
+                    }
+                }
+            } else {
+                for (var tries = 1000; tries--; ) {
+                    rnd = Math.random() * (p.max - p.min) + p.min;
+                    rnd = parseFloat( rnd.toFixed(fract_digits) );
+                    if (!history_list.includes(rnd)) {
+                        history_list.push(rnd);
+                        rnd_list.push(rnd);
+                        break;
+                    }
+                }
             }
         } else {
             rnd = Math.random() * (p.max - p.min) + p.min;
             rnd = parseFloat( rnd.toFixed(fract_digits) );
+            history_list.push(rnd);
             rnd_list.push(rnd);
         }
     }
